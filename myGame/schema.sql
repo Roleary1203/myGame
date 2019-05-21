e93818cf74ae5d4f0183f22719f23aad8484c52d
Ryan O'Leary <roleary1203@gmail.com>
2019-05-20 21:47:36 -0400

    adds number of improvements, working on hero stats

--- myGame/schema.sql
+++ myGame/schema.sql
@@ -5,7 +5,7 @@ USE myGame;
 
 CREATE TABLE player(
   id int NOT NULL AUTO_INCREMENT,
-  accountName varchar(16) NOT NULL,
+  accountName varchar(16) NOT NULL UNIQUE,
   password varchar(16) NOT NULL,
   PRIMARY KEY (id)
 )ENGINE = InnoDB;
@@ -13,7 +13,7 @@ CREATE TABLE player(
 
 CREATE TABLE hero(
   hero_id int NOT NULL AUTO_INCREMENT,
-  heroName varchar(16) NOT NULL,
+  heroName varchar(16) NOT NULL UNIQUE,
   role varchar(16) NOT NULL,
   level int NOT NULL DEFAULT 1,
   account_id int NOT NULL,
@@ -24,7 +24,6 @@ CREATE TABLE hero(
 /*  Execute this file from the command line by typing:
  *    mysql -u root < schema.sql
  *  to create the database and the tables.
- DROP DATABASE [IF EXISTS] myGame;
 */
 
 
